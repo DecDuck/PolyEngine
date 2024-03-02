e5ff523b139a0989f961e2f8f7e6260782e52321
DecDuck <declanahofmeyr@gmail.com>
2024-03-02 18:39:55 +1100

    Fixed wordmark

--- components/Wordmark.vue
+++ components/Wordmark.vue
@@ -1,6 +1,3 @@
 <template>
-  <div class="text-emerald-600 inline-flex items-center gap-x-2">
-    <Logo class="w-16 h-16" />
-    <span class="font-bold text-4xl">Polyengine</span>
-  </div>
+  
 </template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,25 @@
 <template>
-    <Wordmark />
+  <div ref="wordmark" class="text-emerald-600 inline-flex items-center gap-x-2">
+    <Logo class="w-16 h-16" />
+    <span class="font-bold text-4xl">Polyengine</span>
+  </div>
+  <img :src="png" />
 </template>
 
 <script setup lang="ts">
+// @ts-expect-error
+import domtoimage from "dom-to-image";
+
+const wordmark = ref();
+const png = ref();
+
+onMounted(() => {
+  console.log(wordmark.value);
+  domtoimage.toPng(wordmark.value).then((dataUrl: string) => {
+    png.value = dataUrl;
+  });
+});
+
 definePageMeta({
   layout: false,
 });
